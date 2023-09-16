a5570723dec02f9d63c86f50ec5d83338c2fdb30
Eduardo Vítor <eduardovitor730@gmail.com>
2023-09-16 15:28:05 -0300

    feat: backup only once a day without using cron

--- backup.sh
+++ backup.sh
@@ -4,18 +4,26 @@ base_src_dir="/home/eduardovitor"
 base_dst_dir="file:///home/eduardovitor/MEGAsync/Backups"
 keyword="-back"
 
-# First backup Documents dir
 src_docs_folder="${base_src_dir}/Documents"
 dst_docs_folder="${base_dst_dir}/DocumentsDir"
 
-duplicity --include-regexp ${keyword} --no-encryption --exclude "**" ${src_docs_folder} ${dst_docs_folder}
-
-# Second backup Downloads dir
 src_down_folder="${base_src_dir}/Downloads"
 dst_down_folder="${base_dst_dir}/DownloadsDir"
-duplicity --include-regexp ${keyword} --no-encryption --exclude "**" ${src_down_folder} ${dst_down_folder}
 
-# Third backup Desktop dir
-src_desk_folder="${base_src_dir}/Desktop"
-dst_desk_folder="${base_dst_dir}/DesktopDir"
-duplicity --include-regexp ${keyword} --no-encryption --exclude "**" ${src_desk_folder} ${dst_desk_folder}
+# Check last backup date
+last_backup_date=$(cat ${base_src_dir}/last_backup_date)
+
+# Get current date
+current_date=$(date "+%Y-%m-%d")
+
+# Check whether a backup was done today
+if [ "$last_backup_date" != "$current_date" ]; then
+    # First backup Documents dir
+    duplicity --include-regexp ${keyword} --no-encryption --exclude "**" ${src_docs_folder} ${dst_docs_folder}
+    # Second backup Downloads dir
+    duplicity --include-regexp ${keyword} --no-encryption --exclude "**" ${src_down_folder} ${dst_down_folder}
+    # Update last backup's date
+    echo "$current_date" > ${base_src_dir}/last_backup_date
+else
+    echo "A backup was already done today"
+fi
